2adff5373224d260ba7ec7ec13fb613086092fa7
chensj <chensj@hyxt.com>
2016-12-05 16:04:53 +0800

    update Spring Boot from 1.3.2 to 1.4.2 update Spring Data Neo4j from 4.0.0 to 4.1.5

--- data/src/main/java/com/test/data/config/Neo4jConfig.java
+++ data/src/main/java/com/test/data/config/Neo4jConfig.java
@@ -4,8 +4,6 @@ import org.neo4j.ogm.session.SessionFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
-import org.springframework.data.neo4j.server.Neo4jServer;
-import org.springframework.data.neo4j.server.RemoteServer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
@@ -13,10 +11,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EnableNeo4jRepositories(basePackages = { "com.test.data.repositories" })
 public class Neo4jConfig extends Neo4jConfiguration {
-    @Override
-    public Neo4jServer neo4jServer() {
-        return new RemoteServer("http://192.168.1.221:7474","neo4j","12345678");
-    }
+//SDN 升级到4.1.5，连接服务器的配置改在ogm.properties中设定，这样可以访问Neo4j 2.x 到 3.x 版本
+//    @Override
+//    public Neo4jServer neo4jServer() {
+//        return new RemoteServer("http://192.168.1.211:7474","neo4j","12345678");
+//    }
 
     @Override
     public SessionFactory getSessionFactory() {
